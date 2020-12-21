1932a53c5b93c6b9874a43a97a699416917eddb0
Jens Peder Meldgaard <jenspederm@gmail.com>
2020-12-21 08:27:29 +0100

    Fixed typos in description

--- BattleshipFieldValidator.py
+++ BattleshipFieldValidator.py
@@ -14,7 +14,7 @@ battlefield_test = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
 
 def validate_battlefield(field):
     """
-    Write a method that takes a field for well-known board game "Battleship"
+    Write a method that takes a field for well-known game game "Battleship"
     as an argument and returns true if it has a valid disposition of ships,
     false otherwise. Argument is guaranteed to be 10*10 two-dimension array.
     Elements in the array are numbers, 0 if the cell is free and 1 if
@@ -27,7 +27,7 @@ def validate_battlefield(field):
     number of ships may differ from version to version. In this kata we will
     use Soviet/Russian version of the game.
 
-    Before the game begins, players set up the board and place the ships
+    Before the game begins, players set up the game and place the ships
     accordingly to the following rules:
     - There must be single battleship (size of 4 cells), 2 cruisers (size 3),
     3 destroyers (size 2) and 4 submarines (size 1). Any additional ships are
