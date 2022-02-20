35b93fbd70ac232d1dae4650e99bc7bc82b2eedd
JensPederM <jenspederm@gmail.com>
2022-02-20 16:49:17 +0000

    parseInt initial commit

---
+++ parseInt/main.py
@@ -0,0 +1,50 @@
+def parseInt(number: str):
+    units = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ]
+
+    tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ]
+
+    scale = [
+        "nineteen",
+        "hundred",
+        "thousand",
+        "million",
+    ]
+
+    for number in number.replace("-", " ").split():
+        pass
+
+
+if __name__ == "__main__":
+    pass
